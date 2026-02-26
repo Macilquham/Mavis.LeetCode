f884af667e8bd681adb3a0976dcbd48e6f3c5683
Macilquham <kronfield@hotmail.com>
2026-02-26 21:43:48 +1300

    mucking around

--- Mavis.LeetCode.Solutions/LeetCodeOne.cs
+++ Mavis.LeetCode.Solutions/LeetCodeOne.cs
@@ -1,7 +1,22 @@
-﻿namespace Mavis.LeetCode.LeetCodeOne
+﻿namespace Mavis.LeetCode.Solutions
 {
     public class LeetCodeOne
     {
+		public void MergeTwoLists(ListNode list1)
+		{
+            
+		}
+	}
 
-    }
+	
+    //Definition for singly-linked list.
+	public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+ 
 }
