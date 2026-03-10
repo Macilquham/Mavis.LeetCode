bd4394998fbb42c5f7670c401ff2864ed1ae3d2f
Macilquham <kronfield@hotmail.com>
2026-03-10 15:29:28 +1300

    latest change

--- Mavis.LeetCode.Solutions/LeetCodeTwo.cs
+++ Mavis.LeetCode.Solutions/LeetCodeTwo.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Mavis.LeetCode.Solutions
+﻿namespace Mavis.LeetCode.Solutions
 {
-    internal class LeetCodeTwo
+    public class LeetCodeTwo
     {
+        public int[] PlusOne(int[] digits)
+        {
+            if (digits[digits.Length - 1] < 9)
+            {
+                digits[digits.Length - 1] = digits[digits.Length - 1] + 1;
+
+                return digits;
+            }
+
+            long newNum = 0;
+            foreach (var num in digits)
+            {
+                newNum = (long)(newNum * 10 + num);
+            }
+
+            newNum++;
+            return ToIntArray(newNum);
+        }
+
+        private int[] ToIntArray(long newNum)
+        {
+            List<int> digits = [];
+            for (; newNum != 0; newNum /= 10)
+            {
+
+                digits.Add((int)(newNum % 10));
+            }
+
+            digits.Reverse();
+            return digits.ToArray();
+        }
     }
+
+
 }
--- Mavis.LeetCode.Tests/LeetCodeTwoTests.cs
+++ Mavis.LeetCode.Tests/LeetCodeTwoTests.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Mavis.LeetCode.Solutions;
+using Shouldly;
 
 namespace Mavis.LeetCode.Tests
 {
-    internal class LeetCodeTwoTests
+    public class LeetCodeTwoTests
     {
+        [Fact]
+		public void TestOne()
+		{
+			LeetCodeTwo leetCodeTwo = new();
+			leetCodeTwo.PlusOne([1,2,3]).ShouldBe([1,2,4]);
+		}
+
+
+		[Fact]
+		public void TestTwo()
+		{
+			LeetCodeTwo leetCodeTwo = new();
+			leetCodeTwo.PlusOne([1,2,9]).ShouldBe([1,3,0]);
+		}
+
+		[Fact]
+		public void TestThree()
+		{
+			LeetCodeTwo leetCodeTwo = new();
+			leetCodeTwo.PlusOne([1,2,3,4,5,6,7,8,9,9,9,9,9]).ShouldBe([1,2,3,4,5,6,7,9,0,0,0,0,0]);
+		}
     }
 }
