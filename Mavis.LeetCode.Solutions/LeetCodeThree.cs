7975b09e3155f0a08e29778e59749549379127e5
Macilquham <kronfield@hotmail.com>
2026-03-10 16:15:59 +1300

    oplaceholder for next leetcode challenge

---
+++ Mavis.LeetCode.Solutions/LeetCodeThree.cs
@@ -0,0 +1,13 @@
+﻿namespace Mavis.LeetCode.Solutions
+{
+	/// <summary>
+	/// https://leetcode.com/problems/climbing-stairs/
+	/// </summary>
+	public class LeetCodeThree
+	{
+		public int ClimbStairs(int n)
+		{
+			return n;
+		}
+	}
+}
---
+++ Mavis.LeetCode.Tests/LeetCodeThreeTests.cs
@@ -0,0 +1,28 @@
+﻿using Mavis.LeetCode.Solutions;
+using Shouldly;
+
+namespace Mavis.LeetCode.Tests
+{
+    public class LeetCodeThreeTests
+    {
+        [Fact]
+		public void TestOne()
+		{
+		}
+
+		[Fact]
+		public void TestTwo()
+		{
+		}
+
+		[Fact]
+		public void TestThree()
+		{
+		}
+
+		[Fact]
+		public void TestFour()
+		{
+		}
+	}
+}
