fcab9a0706b56db87cee2c9a138d7f7556bcbc2a
Macilquham <kronfield@hotmail.com>
2026-03-27 22:40:56 +1300

    leetcode four

---
+++ Mavis.LeetCode.Solutions/LeetCodeFour.cs
@@ -0,0 +1,32 @@
+﻿namespace Mavis.LeetCode.Solutions
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+    /// </summary>
+    public class LeetCodeFour
+    {
+        public int StrStr(string haystack, string needle) {
+            var haystackAsSpan = haystack.AsSpan();
+            var needleAsSpan = needle.AsSpan();
+            
+            if(needleAsSpan.Length > haystackAsSpan.Length)
+            {
+                return -1;
+            }
+
+            for (var haystackPosition = 0; haystackPosition < haystackAsSpan.Length; haystackPosition++) 
+            {
+                if(haystackPosition + needleAsSpan.Length > haystackAsSpan.Length)
+                {
+                    return -1;
+                }
+                else if(haystackAsSpan[haystackPosition] == needleAsSpan[0] && haystackAsSpan.Slice(haystackPosition, needleAsSpan.Length).SequenceEqual(needleAsSpan))
+                {
+                    return haystackPosition;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Mavis.LeetCode.Tests/LeetCodeFourTests.cs
@@ -0,0 +1,56 @@
+﻿using Mavis.LeetCode.Solutions;
+using Shouldly;
+
+namespace Mavis.LeetCode.Tests
+{
+    public class LeetCodeFourTests
+    {
+        [Fact]
+		public void TestOne()
+		{
+			var leetCodeFour = new LeetCodeFour();
+			var result = leetCodeFour.StrStr("foobarfoo", "bar");
+			result.ShouldBe(3);
+		}
+
+		[Fact]
+		public void TestTwo()
+		{
+			var leetCodeFour = new LeetCodeFour();
+			var result = leetCodeFour.StrStr("barfoo", "bar");
+			result.ShouldBe(0);
+		}
+
+		[Fact]
+		public void TestThree()
+		{
+			var leetCodeFour = new LeetCodeFour();
+			var result = leetCodeFour.StrStr("barfoo", "bara");
+			result.ShouldBe(-1);
+		}
+
+		[Fact]
+		public void TestFour()
+		{
+			var leetCodeFour = new LeetCodeFour();
+			var result = leetCodeFour.StrStr("barfofoo", "foo");
+			result.ShouldBe(5);
+		}
+
+		[Fact]
+		public void TestFive()
+		{
+			var leetCodeFour = new LeetCodeFour();
+			var result = leetCodeFour.StrStr("leetcode", "leeto");
+			result.ShouldBe(-1);
+		}
+
+		[Fact]
+		public void TestSix()
+		{
+			var leetCodeFour = new LeetCodeFour();
+			var result = leetCodeFour.StrStr("mississippi", "issip");
+			result.ShouldBe(4);
+		}
+	}
+}
