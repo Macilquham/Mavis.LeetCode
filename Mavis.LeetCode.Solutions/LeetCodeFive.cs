4281cf9e80b76920fc9b7c72e05b3e7c6ad7ba1c
Macilquham <kronfield@hotmail.com>
2026-03-30 22:13:50 +1300

    leetcode five

---
+++ Mavis.LeetCode.Solutions/LeetCodeFive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mavis.LeetCode.Solutions
+{
+    /// <summary>
+    /// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+    /// </summary>
+    public class LeetCodeFive
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var nextIndexToAddEntry = 1;
+            var currentValue = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != currentValue)
+                {
+                    nums[nextIndexToAddEntry] = nums[i];
+                    nextIndexToAddEntry++;
+                    currentValue = nums[i];
+                }
+            }
+
+            return nextIndexToAddEntry;
+        }
+    }
+}
---
+++ Mavis.LeetCode.Tests/LeetCodeFiveTests.cs
@@ -0,0 +1,34 @@
+﻿using Mavis.LeetCode.Solutions;
+using Shouldly;
+
+namespace Mavis.LeetCode.Tests
+{
+    public class LeetCodeFiveTests
+    {
+        [Fact]
+		public void TestOne()
+		{
+			var leetCodeFour = new LeetCodeFive();
+            int[] input = [1, 1, 2];
+            int result = leetCodeFour.RemoveDuplicates(input);
+			result.ShouldBe(2);
+		}
+
+		[Fact]
+		public void TestTwo()
+		{
+			var leetCodeFour = new LeetCodeFive();
+			int[] input = [0,0,1,1,1,2,2,3,3,4];
+			var result = leetCodeFour.RemoveDuplicates(input);
+			result.ShouldBe(5);
+		}
+
+		[Fact]
+		public void TestThree() {
+			var leetCodesFour = new LeetCodeFive();
+			int[] input = [0,0,0,0,3];
+			var result = leetCodesFour.RemoveDuplicates(input);
+			result.ShouldBe(2);
+		}
+	}
+}
