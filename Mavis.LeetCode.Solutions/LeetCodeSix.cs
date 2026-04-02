91f7b878bc4b311c543aedf32f6280ca65bf9fc1
Macilquham <kronfield@hotmail.com>
2026-04-03 09:22:17 +1300

    .

---
+++ Mavis.LeetCode.Solutions/LeetCodeSix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mavis.LeetCode.Solutions
+{
+    public class LeetCodeSix
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+
+            if(nums.Length == 0){ return 0; }
+            
+            if(nums.Length == 1)
+            {
+                return nums[0] == val ? 0 : 1;
+            }
+
+            int numberOfDuplicateFound = 0;
+
+            for (int i = 0; i <= nums.Length / 2 + 1; i++)
+            {
+                if (i == nums.Length)
+                {
+                    return nums.Length - numberOfDuplicateFound;
+                }
+
+                if (nums[i] == val)
+                {
+                    while (nums[nums.Length - 1 - numberOfDuplicateFound] == val)
+                    {
+                        numberOfDuplicateFound++;
+
+                        if (numberOfDuplicateFound == nums.Length)
+                        {
+                            return 0;
+                        }
+                    }
+
+                    if (numberOfDuplicateFound + i == nums.Length)
+                    {
+                        return nums.Length - numberOfDuplicateFound;
+                    }
+
+                    nums[i] = nums[nums.Length - 1 - numberOfDuplicateFound];
+                    nums[nums.Length - 1 - numberOfDuplicateFound] = val;
+                }
+            }
+
+            for (int j = nums.Length / 2; j < nums.Length; j++)
+            {
+                if (nums[j] == val)
+                {
+                    return j;
+                }
+            }
+
+            return nums.Length;
+        }
+    }
+}
---
+++ Mavis.LeetCode.Tests/LeetCodeSixTests.cs
@@ -0,0 +1,93 @@
+﻿using Mavis.LeetCode.Solutions;
+using Shouldly;
+
+namespace Mavis.LeetCode.Tests
+{
+    public class LeetCodeSixTests
+    {
+        [Fact]
+		public void TestOne()
+		{
+			var leetCodeSix = new LeetCodeSix();
+            int[] input = [3,2,2,3];
+            int result = leetCodeSix.RemoveElement(input, 3);
+			result.ShouldBe(2);
+		}
+
+		[Fact]
+		public void TestTwo()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [0,1,2,2,3,0,4,2];
+			int result = leetCodeSix.RemoveElement(input, 2);
+			result.ShouldBe(5);
+		}
+
+		[Fact]
+		public void TestThree()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [0,1,2,2,2];
+			int result = leetCodeSix.RemoveElement(input, 2);
+			result.ShouldBe(2);
+		}
+
+		[Fact]
+		public void TestFour()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [2,2,2,2,1];
+			int result = leetCodeSix.RemoveElement(input, 2);
+			result.ShouldBe(1);
+		}
+
+
+		[Fact]
+		public void TestFive()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [2,2,2,2,1,2,2,2];
+			int result = leetCodeSix.RemoveElement(input, 2);
+			result.ShouldBe(1);
+
+		}
+
+		[Fact]
+		public void TestSix()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [3,3];
+			int result = leetCodeSix.RemoveElement(input, 3);
+			result.ShouldBe(0);
+		}
+
+		[Fact]
+		public void TestSeven()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [1,2,3,4];
+			int result = leetCodeSix.RemoveElement(input, 3);
+			result.ShouldBe(3);
+		}
+
+		[Fact]
+		public void TestEight()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [4,2,0,2,2,1,4,4,1,4,3,2];
+			int result = leetCodeSix.RemoveElement(input, 4);
+			result.ShouldBe(8);
+		}
+
+		[Fact]
+		public void TestNine()
+		{
+			var leetCodeSix = new LeetCodeSix();
+			int[] input = [3,3];
+			int result = leetCodeSix.RemoveElement(input, 5);
+			result.ShouldBe(2);
+		}
+	}
+
+	public record Foo(string Name, string Address);
+}
